4c8bc5dcc55d66cef234b7236341e7dfe2bfd98a
bogedom <mike.bozhedomov@gmail.com>
2018-06-10 20:10:26 +0300

    deploy to dev server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyChCYPOnObm8zgot8g73tTNPeJjf6LlgdU',
+    authDomain: 'placein-dev.firebaseapp.com',
+    databaseURL: 'https://placein-dev.firebaseio.com',
+    projectId: 'placein-dev',
+    storageBucket: 'placein-dev.appspot.com',
+    messagingSenderId: '460357803597'
+  }
 };
